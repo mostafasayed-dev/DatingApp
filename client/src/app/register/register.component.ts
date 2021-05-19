56060e6c45e135b6174ce3e83e3af0f813b52828
mostafasayed-dev <mostafasayed.fathi@gmail.com>
2021-05-20 01:16:46 +0200

    End of section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,21 +12,64 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
+  maxDate: Date;
+  validationErrors: string[] = [];
 
   constructor(public accountService: AccountService,
-              private toastr: ToastrService) { }
+              private toastr: ToastrService, private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initializeForm(){
+    // this.registerForm = new FormGroup({
+    //   username: new FormControl('', Validators.required),
+    //   password: new FormControl('', [Validators.required, Validators.minLength(4), 
+    //                                  Validators.maxLength(8)]),
+    //   confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    // });
+    // more simple way for creating reactive forms
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), 
+                                     Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    });
+    // when password value change validate confirmPassword value again
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+        this.registerForm.controls.confirmPassword.updateValueAndValidity();
+      })
+  }
+
+  matchValues(toMatch: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      // doesn't match ? return null (no errors) : return isMatch: true (there is an error)
+      return control?.value === control?.parent?.controls[toMatch].value ? null : {isMatch: true};
+    }
   }
 
   register(){
-    this.accountService.register(this.model).subscribe(response => {
+    // console.log(this.registerForm.value);
+    // console.log(this.registerForm);
+    this.accountService.register(this.registerForm.value).subscribe(response => {
       // console.log(response);
-      this.cancel();
+      // this.cancel();
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);
+      // console.log(error);
       // this.toastr.error(error.error);
-      this.toastr.error(error);
+      // this.toastr.error(error);
+      this.validationErrors = error;
     })
   }
 
